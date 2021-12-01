866a3f19cda1a32734bbce319d4aa002414363f3
JoseMoraDev <86jamg@gmai.com>
2021-12-01 09:44:36 +0100

    Escribe aqui (ej.3 ap.3)

--- index.php
+++ index.php
@@ -200,7 +200,7 @@ echo "The current date and time are $DateAndTime.";
                                 <form class="form-inline mr-auto w-100 navbar-search">
                                     <div class="input-group">
                                         <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
+                                            placeholder="Escribe aquí" aria-label="Search"
                                             aria-describedby="basic-addon2">
                                         <div class="input-group-append">
                                             <button class="btn btn-primary" type="button">
